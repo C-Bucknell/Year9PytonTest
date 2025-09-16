314fda9f60c77f9d342248903828ac9146feea4c
Chris Bucknell <58235679+Buckas@users.noreply.github.com>
2025-09-16 13:53:34 +1000

    Task_01a and Test updated and correct

--- test_Task_01a.py
+++ test_Task_01a.py
@@ -1,29 +1,26 @@
 import unittest
 from unittest.mock import patch
+import Task_01a
 
-# Original function to be tested
-def main():
-    var = input('Shout: ')
-    print(f'{var} {var} {var}')
 
 class TestEchoProgram(unittest.TestCase):
 
     @patch('builtins.input', return_value='hello')
     @patch('builtins.print')
     def test_input_called(self, mock_print, mock_input):
-        main()
+        Task_01a.main()
         mock_input.assert_called()
 
     @patch('builtins.input', return_value='hello')
     @patch('builtins.print')
     def test_input_prompt(self, mock_print, mock_input):
-        main()
+        Task_01a.main()
         mock_input.assert_called_with('Shout: ')
 
     @patch('builtins.input', return_value='hello')
     @patch('builtins.print')
     def test_output_correct(self, mock_print, mock_input):
-        main()
+        Task_01a.main()
         mock_print.assert_called_with('hello hello hello')
 
 if __name__ == '__main__':
