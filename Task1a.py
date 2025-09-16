7e2ce63704ea26eb10713e51b90305c94191e11e
Chris Bucknell <99325130+C-Bucknell@users.noreply.github.com>
2025-09-16 11:28:46 +1000

    Create Task1a.py

---
+++ Task1a.py
@@ -0,0 +1,22 @@
+
+#Task 1 Echo Mr Bucknell
+'''
+Task 1 Echo (3 + 2 = 5 marks)
+Part a
+When you shout across a valley you can often hear an echo back. Write a program to echo back whatever you say to it. Your program should work like this:
+=========================
+Shout: hello
+hello hello hello
+=========================  
+'''
+def main():
+  #===============================
+  # Write your code for input here
+  var = input('Shout: ')
+  print(f'{var}x3')
+  
+  # End of your code for input here
+  #===============================
+
+if __name__ == '__main__':
+    main()
